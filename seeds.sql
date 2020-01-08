a0aa127c331e71071d25d6e530b082b8d1a66e51
Justin Abreu <justinabreu92@gmail.com>
2020-01-07 22:43:08 -0600

    basis of seeds done so far

---
+++ seeds.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE database company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    dept_name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DEC(7,0) NOT NULL,
+    dept_id INT NOT NULL,
+    PRIMARY KEY (id)
+
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id)
+
+)
+
+SELECT * FROM top5000;
