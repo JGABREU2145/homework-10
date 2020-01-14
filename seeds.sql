2009fbb96912fcb3fed10956dcf70f83124c9065
Justin Abreu <justinabreu92@gmail.com>
2020-01-13 22:42:12 -0600

    database fixed, need Update functionality, redme, gif

--- seeds.sql
+++ seeds.sql
@@ -12,7 +12,7 @@ CREATE TABLE department (
 CREATE TABLE company_role (
     role_id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DEC(7,0) NOT NULL,
+    salary DEC(7,2) NOT NULL,
     dept_id INT,
     PRIMARY KEY (role_id),
     FOREIGN KEY (dept_id) REFERENCES department(dept_id)
@@ -22,9 +22,20 @@ CREATE TABLE employees (
     emp_id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    emp_role_id INT,
+    emp_role_id INT NOT NULL,
     manager_id INT,
     PRIMARY KEY (emp_id),
     FOREIGN KEY (emp_role_id) REFERENCES company_role(role_id),
-    FOREIGN KEY (manager_id) REFERENCES company_role(role_id)
+    FOREIGN KEY (manager_id) REFERENCES employees(emp_id)
 );
+
+INSERT INTO department (dept_name) VALUES ('Back of House'), ('Front of House'), ('Management');
+INSERT INTO company_role (title, salary, dept_id) VALUES
+('General Manager', 70000.00, 3), 
+('Head Chef', 60000.00, 1),
+('Sous Chef', 50000.00, 1),
+('Cook', 30000.00, 1 ),
+('Dishwasher', 20000.00, 1),                
+('Front of House Manager', 40000.00, 2),
+('Host', 30000.00, 2),
+('Server', 20000.00, 1);
